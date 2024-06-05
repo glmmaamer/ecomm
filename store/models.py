253a0aba283cdffade2087db5be65a317e4f5196
glmmaamer <atackerglm12@gmail.com>
2024-06-05 20:39:33 +0100

    website ecommerce

---
+++ store/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+import datetime
+
+# Create your models here.
+#category in product
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name  = models.CharField(max_length=70)
+    email      = models.EmailField(max_length=50)
+    phone      = models.CharField(max_length=10)
+    password   = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name}{self.last_name}'
+    
+class Product(models.Model):
+    name        = models.CharField(max_length=50)
+    price       = models.DecimalField(default=0 ,decimal_places=2, max_digits=6)
+    category    = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=200, default='', blank=True, null=True )
+    image       = models.ImageField(upload_to='upload/product')
+
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    product  = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address  = models.CharField(max_length=100, default='', blank=True)
+    phone    = models.CharField(max_length=20 , default='',blank=True)
+    date     = models.DateField(default=datetime.datetime.today)
+    city     = models.CharField(max_length=150)
+
+
+    def __str__(self):
+        return self.product
+ 
