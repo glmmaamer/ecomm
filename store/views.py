62f788351dd103b88a92f739d0a7cf0f4b7cef1f
glmmaamer <atackerglm12@gmail.com>
2024-06-06 16:10:55 +0100

    create function login and logout authenticate username and password

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import *
 from django.contrib.auth import authenticate , login, logout
 from django.contrib import messages
@@ -14,7 +14,21 @@ def about(request):
     return render(request, 'about.html')
 
 def login_user(request):
-    return render(request, 'login.html')
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("login for your user admin "))
+            return redirect('home')
+        else:
+            messages.success(request,("error"))
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
 
 def logout_user(request):
-    return render(request, 'logout.html')
+    logout(request)
+    messages.success(request,("you have been logged out"))
+    redirect('home')
