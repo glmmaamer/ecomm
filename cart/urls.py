2fbabf183011a4c3f7e104e054985100ebeac1f4
glmmaamer <atackerglm12@gmail.com>
2024-06-13 14:22:09 +0100

    update website add langeag arabic

--- cart/urls.py
+++ cart/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('cart/',views.summary, name='summary'),
     path('add/',views.cart_add, name='cart_add'),
-    path('update/',views.update_cart, name='update'),
+    path('update/',views.update_cart, name='update_cart'),
     path('delete/',views.delete, name='delete'),
 ]
