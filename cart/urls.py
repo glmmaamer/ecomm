5ab6a96af797b8378a88241fb3f1598b23f8a369
glmmaamer <atackerglm12@gmail.com>
2024-07-07 00:08:08 +0100

    update password

--- cart/urls.py
+++ cart/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cart/',views.summary, name='summary'),
     path('add/',views.cart_add, name='cart_add'),
     path('update/',views.update_cart, name='update_cart'),
     path('delete/',views.delete, name='delete_cart'),
+    path('cart/',views.summary, name='summary'),
+
 ]
