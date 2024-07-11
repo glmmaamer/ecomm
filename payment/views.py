a54573a12e715f7de5b48799c6b0755b64dba8ee
glmmaamer <atackerglm12@gmail.com>
2024-07-11 23:16:48 +0100

    add checkout with app payment

--- payment/views.py
+++ payment/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from cart.cart import Cart
+from .forms import ShippingForm
+from .models import ShippingAddress
 
 # Create your views here.
 
@@ -12,7 +14,18 @@ def checkout(request):
     products = cart.get_prods
     quantity = cart.get_quantites
     totales = cart.cart_total()
-    return render(request, 'payment/checkout.html',{'cart_products':products,
+    if request.user.is_authenticated:
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(request,'payment/checkout.html',{'cart_products':products,
                                                     'cart_quantity':quantity,
-                                                    'cart_total':totales
+                                                    'cart_total':totales,
+                                                    'shipping_form':shipping_form
                                                     })
+    else:
+        shipping_form = ShippingForm(request.POST or None)
+        return render(request, 'payment/checkout.html',{'cart_products':products,
+                                                        'cart_quantity':quantity,
+                                                        'cart_total':totales,
+                                                        'shipping_form':shipping_form
+                                                        })
