c2f44d669f607cfe1dde89c1ef5cf71990e91640
glmmaamer <atackerglm12@gmail.com>
2024-07-11 20:42:37 +0100

    add checkout with app payment

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from cart.cart import Cart
 
 # Create your views here.
 
@@ -7,5 +8,11 @@ def payment_success(request):
     return render(request, "payment/payment.html")
 
 def checkout(request):
-
-    return render(request, 'payment/checkout.html')
+    cart = Cart(request)
+    products = cart.get_prods
+    quantity = cart.get_quantites
+    totales = cart.cart_total()
+    return render(request, 'payment/checkout.html',{'cart_products':products,
+                                                    'cart_quantity':quantity,
+                                                    'cart_total':totales
+                                                    })
