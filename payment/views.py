6b346acde14f20867dcd13aca67992834406f96b
glmmaamer <atackerglm12@gmail.com>
2024-07-13 11:03:09 +0100

    add page processing order

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from cart.cart import Cart
 from .forms import ShippingForm, PaymentForm
-from .models import ShippingAddress
+from .models import ShippingAddress, Order, OrderItem
+from django.contrib.auth.models import User
 from django.contrib import messages
+
 
 # Create your views here.
 
@@ -36,8 +38,8 @@ def billin_info(request):
         products = cart.get_prods
         quantity = cart.get_quantites
         totales = cart.cart_total()
-        my_billing = request.POST
-        request.session['my_billing'] = my_billing
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
         if request.user.is_authenticated:
             billing_form = PaymentForm()
             return render(request, 'payment/billing_info.html',{'cart_products':products,
@@ -68,12 +70,32 @@ def billin_info(request):
         return redirect('home')
     
 def process_order(request):
+
     if request.POST:
+        cart = Cart(request)
+        products = cart.get_prods
+        quantity = cart.get_quantites
+        totals = cart.cart_total()
         payment_form = PaymentForm(request.POST or None)
-        my_billing = request.session.get('my_billing')
-        print(my_billing)
-        messages.success(request,'يتم  معالجة الطلب')
-        return redirect('home')
+        my_shipping = request.session.get('my_shipping')
+        full_name = my_shipping['shipping_full_name']
+        email = my_shipping['shipping_email']
+
+        shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_country']}"
+        amount_paid = totals
+        if request.user.is_authenticated:
+            user = request.user
+            create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+            messages.success(request,'يتم  معالجة الطلب')
+            return redirect('home')
+        else:
+            create_order = Order( full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+            messages.success(request,'يتم  معالجة الطلب')
+            return redirect('home')
+
+        
 
     else:
         messages.success(request,'لم يتم إرسال الطلب')
