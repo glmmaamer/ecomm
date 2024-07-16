3400182c27e685765d8b5184a3c57825b240a7eb
glmmaamer <atackerglm12@gmail.com>
2024-07-16 18:48:51 +0100

    update website

--- ecom/settings.py
+++ ecom/settings.py
@@ -134,10 +134,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = 'static/'
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
-
+STATICFILES_DIRS = ['ecom/static']
 
 
 
